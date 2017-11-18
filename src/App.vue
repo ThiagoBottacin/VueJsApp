ad3c53a2fc1a77937dda8dcca4b35700a0da8440
ThiagoBottacin <thiago.bottacin@gmail.com>
2017-11-18 12:56:45 -0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <CcHello></CcHello>
+  </div>
+</template>
+
+<script>
+import CcHello from './components/HelloWorld.vue'
+
+export default {
+  name: 'webschoolComponent',
+
+  components: {
+    CcHello
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Intro.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed purus erat. In consectetur, tellus nec malesuada consequat, lorem lorem lacinia ipsum, sit amet efficitur tellus neque et nunc. Vivamus aliquet sapien lorem. Suspendisse et lorem maximus, viverra metus non, aliquet velit. Ut porta elit ac risus eleifend eleifend. Praesent vitae viverra ligula. Vestibulum fermentum tincidunt erat, nec venenatis quam malesuada id. Suspendisse ut justo mauris. Cras lacinia semper viverra. Etiam imperdiet neque vitae mauris auctor, quis venenatis mi vestibulum.
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+p{
+    color: blue;
+}
+</style>
